29105b88e4520936a1a96ec37e41e0f21bfb4ea8
anarsantos <aninha_rsantos@yahoo.com.br>
2023-08-10 13:01:48 -0300

    modificadores de acesso e herança

---
+++ src/seguros/SeguroPessoaFisica.java
@@ -0,0 +1,13 @@
+package seguros;
+
+import contas.Conta;
+
+public class SeguroPessoaFisica extends Conta {
+    public static void main(String[] args) {
+        SeguroPessoaFisica cc3 = new SeguroPessoaFisica();
+        cc3.cliente = "Robson Vaamonte";
+        cc3.saldo = 9000;
+        System.out.println("Cliente: " + cc3.cliente);
+        cc3.exibirSaldo();
+    }
+}
